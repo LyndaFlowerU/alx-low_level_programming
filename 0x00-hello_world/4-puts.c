6179811037fbab4775600549f5b54698354e476c
LyndaFlowerU <umurerwalyndaflower@gmail.com>
2023-08-14 08:25:06 -0700

    scripts that prints lines with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Retuen: 0(Success)
+ * main - Program that prits a lines with puts function
+ *
+ * Return: 0 (Success)
  */
-int main(void){
-	puts("/"Programming is like building a multilingual puzzle");
-	return(0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 };
