6626b1aef65a33ed96c0215f8f9383e483c8ff30
LyndaFlowerU <umurerwalyndaflower@gmail.com>
2023-08-14 09:20:47 -0700

    script that prints lines using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - A program that prints a lines using printf
  * return: 0(Success)
  */
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
