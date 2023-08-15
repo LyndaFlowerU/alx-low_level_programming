1dac1b1e6c716afe2f4de865d6803b1d8fa63030
LyndaFlowerU <umurerwalyndaflower@gmail.com>
2023-08-14 18:46:41 -0700

    modify lines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 
 	printf("size of char:%lu byte(s)\n)", sizeof(a));
 	printf("size of int:%lu byte(s)\n", sizeof(b));
-	printf("size of long int:%lu byte(s)\n", sizeof(c));
-	printf("size of long long int:%lu byte(s)\n", sizeof(x));
-	printf("size of float:%lu byte(s)\n", sizeof(y));
+	printf("size of long int:%d byte(s)\n", sizeof(c));
+	printf("size of long long int:%d byte(s)\n", sizeof(x));
+	printf("size of float:%d byte(s)\n", sizeof(y));
 	return (0);
 }
